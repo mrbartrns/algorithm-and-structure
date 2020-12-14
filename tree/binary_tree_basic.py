eadea5727720409c4d1b52d40398a90eb6388bbc
Junhyuck <mrbartrns@naver.com>
2020-12-14 16:52:38 +0900

    tree basic

---
+++ tree/binary_tree_basic.py
@@ -0,0 +1,96 @@
+"""
+tree 자료구조 만들기(이진탐색 트리 구현)
+"""
+
+
+class Node:
+    """
+    1. node는 데이터와 childNode의 참조값 두개를 저장한다.
+    2. size와 depth는 재귀적으로 구현한다. 재귀는, 귀납적으로 생각하여 메소드의 값이 해결되었다고 가정하고 코드를 작성해나간다. (종결조건만 잘 잡아주면 된다)
+    """
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def size(self):
+        l = self.left.size() if self.left else 0
+        r = self.right.size() if self.right else 0
+        return l + r + 1
+
+    def depth(self):
+        left_depth = self.left.depth() if self.left else 0
+        right_depth = self.right.depth() if self.right else 0
+        return left_depth + 1 if left_depth > right_depth else right_depth + 1
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class BinaryTree:
+    """
+    class BinaryTree는 node클래스를 기본으로 받는다고 가정하고 코드를 작성한다.
+    """
+    def __init__(self):
+        self.root = None
+    
+    def size(self):
+        return self.root.size() if self.root else 0
+    
+    def depth(self):
+        return self.root.depth() if self.root else 0
+
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+    
+    def find(self, key):
+        return self._find_value(self.root, key)
+
+    def _find_value(self, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)
+    
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+    
+    def _delete_value(self, node, key):
+        if node is None:
+            return node, False
+        
+        deleted = False
+        if key == node.data::
+            deleted = True
+            if node.left and node.right:
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            elif node.left or node.right:
+                node = node.left or node.right
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+            node.right, deleted = self._delete_vale(node.right, key)
+        return node, deleted
