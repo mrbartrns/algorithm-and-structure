f6ac8a0df19da285494278625a51d2f5b4698e27
Junhyuck <mrbartrns@naver.com>
2020-12-16 18:36:41 +0900

    값 찾는 함수 수정

--- tree/tree_p1.py
+++ tree/tree_p1.py
@@ -18,14 +18,15 @@ class Tree:
     def insert(self, parent, child):
         if not self.root:
             self.root = Node(parent)
-        self.root = self._insert_value(self.root, parent, child)
+        visited = [False]
+        self.root = self._insert_value(self.root, parent, child, visited)
         return self.root is not None
 
-    def _insert_value(self, node, parent, child, visited=False):
+    def _insert_value(self, node, parent, child, visited):
 
         if not node:
             node = Node(child)
-            visited = True
+            visited[0] = True
 
         else:
             if node.data == parent and (not node.left or not node.right):
@@ -34,43 +35,11 @@ class Tree:
                 elif not node.right:
                     node.right = self._insert_value(node.right, parent, child, visited)
             else:
-                if not visited and node.left:
-                    node.left = self._insert_value(node.left, parent, child)
-                if not visited and node.right:
-                    node.right = self._insert_value(node.right, parent, child)
+                if not visited[0] and node.left:
+                    node.left = self._insert_value(node.left, parent, child, visited)
+                if not visited[0] and node.right:
+                    node.right = self._insert_value(node.right, parent, child, visited)
         return node
-
-    def size(self, key):
-        node = self.find(key)
-        return node.size() if node else 0
-
-    def find(self, key):
-        """
-        data를 찾는 함수
-        help function
-        input: key
-        return: boolean if node.data == key or node is None
-        """
-        return self._find_value(self.root, key)
-
-    def _find_value(self, node, key):
-        """
-        data를 찾기 위한 재귀함수
-        사용시에는 find 함수만 사용할 것
-        input: node, key (init: self.root)
-        return: recursive function
-        """
-        if not node:
-            return None
-        elif node.data == key:
-            return node
-
-        else:
-            if key < node.data:
-                node = self._find_value(node.left, key)
-            else:
-                node = self._find_value(node.right, key)
-            return node
 
     def preorder(self, node):
         if node is not None:
@@ -108,18 +77,40 @@ for tc in range(t):
     print(f"#{tc + 1} {tree.size(sub)}")
 """
 
+
+def find_value(node, value, counts, visited):
+    if node.data == value:
+        counts[0] += 1
+        counts[1] = True
+
+    elif counts[1] and node.data not in visited:
+        counts[0] += 1
+
+    if node is not None:
+        if node.data not in visited:
+            visited.append(node.data)
+        if node.left:
+            find_value(node.left, value, counts, visited)
+        if node.right:
+            find_value(node.right, value, counts, visited)
+        if node.data == value:
+            counts[1] = False
+
+
 # data = [2, 1, 2, 5, 1, 6, 5, 3, 6, 4]
 # data = [2, 6, 6, 4, 6, 5, 4, 1, 5, 3]
-# data = [7, 6, 7, 4, 6, 9, 4, 11, 9, 5, 11, 8, 5, 3, 5, 2, 8, 1, 8, 10]
-data = [1, 1, 1, 1, 1, 1, 1, 1]
+data = [7, 6, 7, 4, 6, 9, 4, 11, 9, 5, 11, 8, 5, 3, 5, 2, 8, 1, 8, 10]
+# data = [2, 1, 2, 1, 1, 2, 1, 2]
 tree = Tree()
 for i in range(0, len(data), 2):
     parent = data[i]
     child = data[i + 1]
     tree.insert(parent, child)
-    tree.levelorder(tree.root)
-    print()
-# print(tree.size(5))
-
+# tree.levelorder(tree.root)
+# print()
+visited = []
+counts = [0, False]
+find_value(tree.root, 1, counts, visited)
+print(counts[0])
 
 # tree.preorder(tree.root)
