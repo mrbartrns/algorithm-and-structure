6093a6521f6f1584ae2fd00a92faccdb1dcaffd7
Junhyuck <mrbartrns@naver.com>
2021-01-20 21:51:33 +0900

    3N 타일 채우기

---
+++ dp_easy/3tile.py
@@ -0,0 +1,26 @@
+# BOJ 2133번
+import sys
+
+input = sys.stdin.readline
+
+
+def solve(n):
+    if n >= 2:
+        memo[2] = 3
+
+    if n % 2 == 0:
+        for i in range(4, n + 1, 2):
+            memo[i] = memo[i - 2] * memo[2]
+            for j in range(4, i, 2):
+                memo[i] += memo[i - j] * 2
+            memo[i] += 2
+    return memo[n]
+
+
+n = int(input())
+memo = [0] * (n + 1)
+memo[0] = 1
+if n >= 2:
+    memo[2] = 3
+
+print(solve(n))
