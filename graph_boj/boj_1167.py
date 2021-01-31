b8449e18a2742646ee83c11dfc7d55a9eda59c76
Junhyuck <mrbartrns@naver.com>
2021-01-31 19:47:31 +0900

    1167번 복습

---
+++ graph_boj/boj_1167.py
@@ -0,0 +1,21 @@
+n = 5
+graph = [[], [[3, 2]], [[4, 4]], [[1, 2], [4, 3]], [[2, 4], [3, 3], [5, 6]], [[4, 6]]]
+visited = [False] * (n + 1)
+distance = [0] * (n + 1)
+
+
+def dfs(v):
+    visited[v] = True
+    for i, j in graph[v]:
+        if not visited[i]:
+            distance[i] = distance[v] + j
+            dfs(i)
+
+
+dfs(1)
+max_d = distance.index(max(distance))
+
+visited = [False] * (n + 1)
+distance = [0] * (n + 1)
+dfs(max_d)
+print(max(distance))
