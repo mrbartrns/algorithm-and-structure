b9654025f093dd95ca0d93242496317868eb1a75
Junhyuck <mrbartrns@naver.com>
2021-02-01 10:52:22 +0900

    test

--- graph_boj/test.py
+++ graph_boj/test.py
@@ -1,5 +1,14 @@
-# BOJ 1427
+# BOJ 1181
+n = int(input())
+arr = []
+check = {}
+for _ in range(n):
+    c = input()
+    arr.append(c)
+    check[c] = False
 
-arr = list(input())
-arr.sort(reverse=True)
-print("".join(arr))
+arr.sort(key=lambda x: (len(x), x))
+for i in range(len(arr)):
+    if not check[arr[i]]:
+        print(arr[i])
+        check[arr[i]] = True
