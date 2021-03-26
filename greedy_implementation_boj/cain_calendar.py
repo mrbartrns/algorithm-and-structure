29e11e8ae959d46cd7995d0aa339e50efea20f39
Junhyuck <mrbartrns@naver.com>
2021-03-26 17:55:21 +0900

    완전탐색

---
+++ greedy_implementation_boj/cain_calendar.py
@@ -0,0 +1,33 @@
+# BOJ 6064
+import sys
+
+si = sys.stdin.readline
+
+
+# 방법 1. 최소공배수를 이용하기
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+# 최소공배수 구하기
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+t = int(si())
+for _ in range(t):
+    m, n, x, y = map(int, si().split())
+    max_year = lcm(m, n)
+    while 1:
+        if x > max_year or (x - 1) % n + 1 == y:
+            break
+        x += m
+
+    if x > max_year:
+        print(-1)
+    else:
+        print(x)
