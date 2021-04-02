ab64602af72a2d1cf7c7062b686ae52ecadfc20f
Junhyuck <mrbartrns@naver.com>
2021-04-02 18:53:09 +0900

    implementation

--- greedy_implementation_boj/ilove.py
+++ greedy_implementation_boj/ilove.py
@@ -6,13 +6,14 @@ si = sys.stdin.readline
 k = int(si())
 n = int(si())
 limit = 210
-arr = [0] * 8
 idx = k - 1
+done = False
 for _ in range(n):
     str_time, check = si().split()
     t = int(str_time)
+    limit -= t
+    if not done and limit <= 0:
+        print(idx + 1)
+        done = True
     if check == 'T':
         idx = (idx + 1) % 8
-    limit -= t
-    if limit <= 0:
-        print(idx + 1)
