ee5f28b666468575a6379e0048040724b773bfa9
Junhyuck <mrbartrns@naver.com>
2021-04-13 09:01:49 +0900

    다이나믹 프로그래밍

--- dp_boj/pipe.py
+++ dp_boj/pipe.py
@@ -6,69 +6,35 @@ si = sys.stdin.readline
 directions = [[0, 1], [1, 1], [1, 0]]
 
 
-def dfs(x, y, mode):
-    if dp[x][y] > -1:
-        return dp[x][y]
+def dfs(y, x, p):
+    if dp[y][x][p] > -1:
+        return dp[y][x][p]
 
-    # 종결조건
     if x == n - 1 and y == n - 1:
-        dp[x][y] = 1
-        return dp[x][y]
-
-    dp[x][y] = 0
-    for i in range(3):
-        nx = x + directions[i][0]
-        ny = y + directions[i][1]
-
-        if nx >= n or ny >= n:
+        dp[y][x][p] = 1
+        return dp[y][x][p]
+    dp[y][x][p] = 0
+    for d in range(3):
+        nx = x + directions[d][1]
+        ny = y + directions[d][0]
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
             continue
-
-        if i == 0 and nx != ny and ny == n - 1:
+        if graph[ny][nx] == 1:
             continue
-
-        if i == 2 and nx != ny and nx == n - 1:
+        if d == 1 and (graph[ny][x] == 1 or graph[y][nx] == 1):
             continue
-
-        if graph[nx][ny] == 1 or (
-            i == 1 and (graph[nx - 1][ny] == 1 or graph[nx][ny - 1] == 1)
-        ):
-            continue
-
-        if mode == 1 or i != 2 - mode:
-            dp[x][y] += dfs(nx, ny, i)
-    return dp[x][y]
+        if p == 0 and d != 2:
+            dp[y][x][p] += dfs(ny, nx, d)
+        elif p == 1:
+            dp[y][x][p] += dfs(ny, nx, d)
+        elif p == 2 and d != 0:
+            dp[y][x][p] += dfs(ny, nx, d)
+    return dp[y][x][p]
 
 
 n = int(si())
+dp = [[[-1 for _ in range(3)] for _ in range(n)] for _ in range(n)]
 graph = [list(map(int, si().split())) for _ in range(n)]
-dp = [[-1 for _ in range(n)] for _ in range(n)]
-print(dfs(0, 1, 0))
-print(dp)
-
-"""
-def dfs(x, y, mode):
-    if dp[x][y] > -1:
-        return dp[x][y]
-
-    if x == n - 1 and y == n - 1:
-        dp[x][y] = 1
-        return dp[x][y]
-
-    dp[x][y] = 0
-    for i in range(3):
-        nx = x + directions[i][0]
-        ny = y + directions[i][1]
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
-        if (graph[nx][ny] == 1) or (
-            i == 1 and graph[nx - 1][ny] == 1 or graph[nx][ny - 1] == 1
-        ):
-            continue
-        if i == 0 and nx != ny and ny == n - 1:
-            continue
-        if i == 2 and nx != ny and nx == n - 1:
-            continue
-        if mode == 1 or i != 2 - mode:
-            dp[x][y] += dfs(nx, ny, i)
-    return dp[x][y]
-"""
+dp[0][0][0] = 0
+dfs(0, 1, 0)
+print(dp[0][1][0])
