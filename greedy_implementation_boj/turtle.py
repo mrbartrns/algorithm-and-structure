675e5acc2fadfca81de1428d86cba11341339790
Junhyuck <mrbartrns@naver.com>
2021-04-13 21:03:16 +0900

    거북이 시뮬레이션 미해결

---
+++ greedy_implementation_boj/turtle.py
@@ -0,0 +1,13 @@
+# BOJ 8911
+import sys
+
+si = sys.stdin.readline
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
+
+# t = int(si())
+# for _ in range(t):
+#     y, x =
+#     order = list(si().strip())
+#     for c in order:
+#         pass
