edb1c10ef9cccd764665ed9b05e40a3e7f762f02
Junhyuck <mrbartrns@naver.com>
2021-04-20 00:03:12 +0900

    큐빙 해결

--- simulation_boj/cubing.py
+++ simulation_boj/cubing.py
@@ -81,9 +81,11 @@ def rotate_current_face(face, op):
 def rotate(op):
     if op[0] == "U":
         d1, d2, d4, d5 = copy(op)
+        # cube[0] = rotate_current_face(cube[0], op[1])
+        new_face = rotate_current_face(cube[0], op[1])
         for i in range(3):
             for j in range(3):
-                cube[0][i][j] = rotate_current_face(cube[0], op[1])[i][j]
+                cube[0][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
                 cube[1][0][i] = d2[i]
@@ -97,15 +99,18 @@ def rotate(op):
                 cube[5][0][i] = d4[i]
     elif op[0] == "D":
         d1, d2, d4, d5 = copy(op)
+        # print(d1, d2, d4, d5)
+        # cube[3] = rotate_current_face(cube[3], op[1])
+        new_face = rotate_current_face(cube[3], op[1])
         for i in range(3):
             for j in range(3):
-                cube[3][i][j] = rotate_current_face(cube[3], op[1])[i][j]
+                cube[3][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
-                cube[1][0][i] = d5[i]
-                cube[2][0][i] = d1[i]
-                cube[4][0][i] = d2[i]
-                cube[5][0][i] = d4[i]
+                cube[1][2][i] = d5[i]
+                cube[2][2][i] = d1[i]
+                cube[4][2][i] = d2[i]
+                cube[5][2][i] = d4[i]
             else:
                 cube[1][2][i] = d2[i]
                 cube[2][2][i] = d4[i]
@@ -113,41 +118,47 @@ def rotate(op):
                 cube[5][2][i] = d1[i]
     elif op[0] == "F":
         d0, d2, d3, d5 = copy(op)
+        # cube[1] = rotate_current_face(cube[1], op[1])
+        new_face = rotate_current_face(cube[1], op[1])
         for i in range(3):
             for j in range(3):
-                cube[1][i][j] = rotate_current_face(cube[1], op[1])[i][j]
+                cube[1][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
-                cube[0][2][i] = d5[i]
+                cube[0][2][i] = d5[2 - i]
                 cube[3][2][i] = d2[i]
                 cube[2][i][0] = d0[i]
                 cube[5][i][2] = d3[2 - i]
             else:
                 cube[0][2][i] = d2[i]
-                cube[3][2][i] = d5[i]
+                cube[3][2][i] = d5[2 - i]
                 cube[2][i][0] = d3[i]
                 cube[5][i][2] = d0[2 - i]
     elif op[0] == "B":
         d0, d2, d3, d5 = copy(op)
+        # cube[4] = rotate_current_face(cube[4], op[1])
+        new_face = rotate_current_face(cube[4], op[1])
         for i in range(3):
             for j in range(3):
-                cube[4][i][j] = rotate_current_face(cube[4], op[1])[i][j]
+                cube[4][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
                 cube[0][0][i] = d2[i]
-                cube[3][0][i] = d5[i]
+                cube[3][0][i] = d5[2 - i]
                 cube[2][i][2] = d3[i]
                 cube[5][i][0] = d0[2 - i]
             else:
-                cube[0][0][i] = d5[i]
+                cube[0][0][i] = d5[2 - i]
                 cube[3][0][i] = d2[i]
                 cube[2][i][2] = d0[i]
                 cube[5][i][0] = d3[2 - i]
     elif op[0] == "L":
+        # cube[5] = rotate_current_face(cube[5], op[1])
+        new_face = rotate_current_face(cube[5], op[1])
         d0, d1, d3, d4 = copy(op)
         for i in range(3):
             for j in range(3):
-                cube[5][i][j] = rotate_current_face(cube[5], op[1])[i][j]
+                cube[5][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
                 cube[0][i][0] = d4[2 - i]
@@ -160,10 +171,12 @@ def rotate(op):
                 cube[3][i][2] = d4[i]
                 cube[4][i][2] = d0[2 - i]
     elif op[0] == "R":
+        # cube[2] = rotate_current_face(cube[2], op[1])
+        new_face = rotate_current_face(cube[2], op[1])
         d0, d1, d3, d4 = copy(op)
         for i in range(3):
             for j in range(3):
-                cube[2][i][j] = rotate_current_face(cube[2], op[1])[i][j]
+                cube[2][i][j] = new_face[i][j]
         for i in range(3):
             if op[1] == "+":
                 cube[0][i][2] = d1[i]
@@ -178,6 +191,7 @@ def rotate(op):
 
 
 t = int(si())
+
 for _ in range(t):
     cube = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]],
             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
@@ -189,6 +203,9 @@ for _ in range(t):
     rotation = list(si().split())
     for c in rotation:
         rotate(c)
+        # for i in range(6):
+        #     print(cube[i])
+        # print()
     for i in range(3):
         for j in range(3):
             print(COLOR[cube[0][i][j]], end="")
