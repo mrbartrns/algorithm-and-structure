530598c1685e6eb1d7264fec2527a964a81d9af4
Junhyuck <mrbartrns@naver.com>
2021-04-28 21:03:42 +0900

    토네이도 상어 시뮬레이션

--- simulation_boj/tornado.py
+++ simulation_boj/tornado.py
@@ -14,31 +14,251 @@ st = (n // 2, n // 2)
 
 
 # 토네이도의 이동 먼저 구현
-def move(start_point):
-    y, x = start_point
-    d = 0
-    scale = 1
-    while True:
-        t = 0
-        print(y, x)
-        if y == 0 and x == 0:
-            break
-
-        if tornado[y][x]:
-            break
-
-        while t < scale:
-            ny = y + dy[d]
-            nx = x + dx[d]
-            if tornado[ny][nx]:
-                break
-            y = ny
-            x = nx
-            t += 1
-
-        d = (d + 1) % 4
-        if d % 2 == 0:
-            scale += 1
+def move(y, x, d, scale):
+    t = 0
+    tornado[y][x] = True
+    while t < scale:
+        ny = y + dy[d]
+        nx = x + dx[d]
+        if not tornado[ny][nx]:
+            tornado[ny][nx] = True
+            y, x = ny, nx
+            # ret[0] += spread(y, x, d)
+            # for i in range(n):
+            #     for j in range(n):
+            #         print(graph[i][j], end=" ")
+            #     print()
+            # print()
+        t += 1
+    return y, x
 
 
-move(st)
+def spread(y, x, d):
+    # 현재 칸의 모든 모래가 사라져야함
+    silicon = graph[y][x]
+    # print(silicon)
+    outer_scope = 0
+    s = 0
+    right = (d + 3) % 4
+    left = (d + 1) % 4
+    # right check
+    if y + dy[right] < 0 or y + dy[right] >= n or x + dx[right] < 0 or x + dx[right] >= n:
+        outer_scope += silicon * 7 // 100
+    else:
+        graph[y + dy[right]][x + dx[right]] += silicon * 7 // 100
+    s += silicon * 7 // 100
+
+    if y + 2 * dy[right] < 0 or y + 2 * dy[right] >= n or x + 2 * dx[right] < 0 or x + 2 * dx[right] >= n:
+        outer_scope += silicon * 2 // 100
+    else:
+        graph[y + 2 * dy[right]][x + 2 * dx[right]] += silicon * 2 // 100
+    s += silicon * 2 // 100
+
+    # left check
+    if y + dy[left] < 0 or y + dy[left] >= n or x + dx[left] < 0 or x + dx[left] >= n:
+        outer_scope += silicon * 7 // 100
+    else:
+        graph[y + dy[left]][x + dx[left]] += silicon * 7 // 100
+    s += silicon * 7 // 100
+
+    if y + 2 * dy[left] < 0 or y + 2 * dy[left] >= n or x + 2 * dx[left] < 0 or x + 2 * dx[left] >= n:
+        outer_scope += silicon * 2 // 100
+    else:
+        graph[y + 2 * dy[left]][x + 2 * dx[left]] += silicon * 2 // 100
+    s += silicon * 2 // 100
+
+    # front check
+    if y + 2 * dy[d] < 0 or y + 2 * dy[d] >= n or x + 2 * dx[d] < 0 or x + 2 * dx[d] >= n:
+        outer_scope += silicon * 5 // 100
+    else:
+        graph[y + 2 * dy[d]][x + 2 * dx[d]] += silicon * 5 // 100
+    s += silicon * 5 // 100
+
+    # check else
+    if d == 0:
+        if y + dy[right] < 0 or y + dy[right] >= n or x + dx[right] - 1 < 0 or x + dx[right] - 1 >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[right]][x + dx[right] - 1] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[right] < 0 or y + dy[right] >= n or x + dx[right] + 1 < 0 or x + dx[right] + 1 >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[right]][x + dx[right] + 1] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+        if y + dy[left] < 0 or y + dy[left] >= n or x + dx[left] - 1 < 0 or x + dx[left] - 1 >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[left]][x + dx[left] - 1] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[left] < 0 or y + dy[left] >= n or x + dx[left] + 1 < 0 or x + dx[left] + 1 >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[left]][x + dx[left] + 1] += silicon * 1 // 100
+        s += silicon * 1 // 100
+    elif d == 1:
+        if y + dy[right] + 1 < 0 or y + dy[right] + 1 >= n or x + dx[right] < 0 or x + dx[right] >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[right] + 1][x + dx[right]] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[right] - 1 < 0 or y + dy[right] - 1 >= n or x + dx[right] < 0 or x + dx[right] >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[right] - 1][x + dx[right]] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+        if y + dy[left] + 1 < 0 or y + dy[left] + 1 >= n or x + dx[left] < 0 or x + dx[left] >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[left] + 1][x + dx[left]] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[left] - 1 < 0 or y + dy[left] - 1 >= n or x + dx[left] < 0 or x + dx[left] >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[left] - 1][x + dx[left]] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+    elif d == 2:
+        if y + dy[right] < 0 or y + dy[right] >= n or x + dx[right] + 1 < 0 or x + dx[right] + 1 >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[right]][x + dx[right] + 1] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[right] < 0 or y + dy[right] >= n or x + dx[right] - 1 < 0 or x + dx[right] - 1 >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[right]][x + dx[right] - 1] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+        if y + dy[left] < 0 or y + dy[left] >= n or x + dx[left] + 1 < 0 or x + dx[left] + 1 >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[left]][x + dx[left] + 1] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[left] < 0 or y + dy[left] >= n or x + dx[left] - 1 < 0 or x + dx[left] - 1 >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[left]][x + dx[left] - 1] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+    elif d == 3:
+        if y + dy[right] - 1 < 0 or y + dy[right] - 1 >= n or x + dx[right] < 0 or x + dx[right] >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[right] - 1][x + dx[right]] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[right] + 1 < 0 or y + dy[right] + 1 >= n or x + dx[right] < 0 or x + dx[right] >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[right] + 1][x + dx[right]] += silicon * 1 // 100
+        s += silicon * 1 // 100
+
+        if y + dy[left] - 1 < 0 or y + dy[left] - 1 >= n or x + dx[left] < 0 or x + dx[left] >= n:
+            outer_scope += silicon * 10 // 100
+        else:
+            graph[y + dy[left] - 1][x + dx[left]] += silicon * 10 // 100
+        s += silicon * 10 // 100
+
+        if y + dy[left] + 1 < 0 or y + dy[left] + 1 >= n or x + dx[left] < 0 or x + dx[left] >= n:
+            outer_scope += silicon * 1 // 100
+        else:
+            graph[y + dy[left] + 1][x + dx[left]] += silicon * 1 // 100
+        s += silicon * 1 // 100
+    # s = silicon * 45 // 100
+    if y + dy[d] < 0 or y + dy[d] >= n or x + dx[d] < 0 or x + dx[d] >= n:
+        outer_scope += silicon - s
+    else:
+        graph[y + dy[d]][x + dx[d]] += silicon - s
+    # print(outer_scope)
+    graph[y][x] = 0
+
+    return outer_scope
+
+
+r, c = st
+d = 0
+scale = 1
+ret = [0]
+# while True:
+#     r, c = move(r, c, d, scale)
+#     print(r, c)
+#     if r == 0 and c == 0:
+#         break
+#     d = (d + 1) % 4
+#     if d % 2 == 0:
+#         scale += 1
+#     print(scale)
+# print(ret[0])
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+r, c = move(r, c, d, scale)
+print(r, c)
+d = (d + 1) % 4
+if d % 2 == 0:
+    scale += 1
+print(scale)
+
+
---
+++ simulation_boj/tornado_2.py
@@ -0,0 +1,68 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+si = sys.stdin.readline
+
+dy = [0, 1, 0, -1]
+dx = [-1, 0, 1, 0]
+sheet = [[(-1, 1, 1), (-1, 0, 7), (-2, 0, 2), (-1, -1, 10), (1, 1, 1), (1, 0, 7), (2, 0, 2), (1, -1, 10), (0, -2, 5)],
+         [(-1, -1, 1), (0, -1, 7), (0, -2, 2), (1, -1, 10), (-1, 1, 1), (0, 1, 7), (0, 2, 2), (1, 1, 10), (2, 0, 5)],
+         [(1, -1, 1), (1, 0, 7), (2, 0, 2), (1, 1, 10), (-1, -1, 1), (-1, 0, 7), (-2, 0, 2), (-1, 1, 10), (0, 2, 5)],
+         [(1, 1, 1), (0, 1, 7), (0, 2, 2), (-1, 1, 10), (1, -1, 1), (0, -1, 7), (0, -2, 2), (-1, -1, 10), (-2, 0, 5)]]
+
+
+def move(y, x, d, scale):
+    t = 0
+    while t < scale:
+        ny = y + dy[d]
+        nx = x + dx[d]
+        y, x = ny, nx
+        # print(y, x)
+        # cnt[0] += spread(y, x, d)
+        t += 1
+    return y, x
+
+
+def spread(y, x, d):
+    over_scope = 0
+    s = 0
+    silicon = graph[y][x]
+    for i in range(9):
+        ty, tx, p = sheet[d][i]
+        ny = y + ty
+        nx = x + tx
+        temp = silicon * p // 100
+        if ny < 0 or ny >= n or nx < 0 or nx >= n:
+            over_scope += temp
+        else:
+            graph[ny][nx] += temp
+        s += temp
+    if y + dy[d] < 0 or y + dy[d] >= n or x + dx[d] < 0 or x + dx[d] >= n:
+        over_scope += silicon - s
+    else:
+        graph[y + dy[d]][x + dx[d]] += silicon - s
+
+    graph[y][x] = 0
+
+    return over_scope
+
+
+n = int(si())
+graph = [list(map(int, si().split())) for _ in range(n)]
+st = (n // 2, n // 2)
+
+r, c = st
+d = 0
+scale = 1
+cnt = [0]
+while True:
+    r, c = move(r, c, d, scale)
+    print(r, c)
+    if r == 0 and c == 0:
+        break
+    d = (d + 1) % 4
+    if d % 2 == 0:
+        scale += 1
+    print(scale)
+
+print(cnt[0])
