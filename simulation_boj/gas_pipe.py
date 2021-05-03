a3ad157535295a3e9b1c120735bcb55f1f33d619
Junhyuck <mrbartrns@naver.com>
2021-05-03 10:00:43 +0900

    가스파이프 시뮬레이션

---
+++ simulation_boj/gas_pipe.py
@@ -0,0 +1,10 @@
+# BOJ 2931
+import sys
+
+sys.stdin = open('../input.txt', 'r')
+si = sys.stdin.readline
+
+n, m = map(int, si().split())
+graph = [list(si().strip()) for _ in range(n)]
+
+print(ascii())
