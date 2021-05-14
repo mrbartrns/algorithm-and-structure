8a284f9ed7f880d2ed61b85f3fe25fab2adcf3f9
Junhyuck <mrbartrns@naver.com>
2021-05-14 22:20:50 +0900

    최적 경로탐색 연습

---
+++ optimal_routing_algorithms/kruskal.py
@@ -0,0 +1,14 @@
+"""
+kruskal algorithm
+이 알고리즘은 그리디 방식으로 문제를 해결한다.
+"""
+
+
+def solution(n, costs):
+    pass
+
+
+if __name__ == "__main__":
+    n = 4
+    costs = [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]
+    print(solution(n, costs))
