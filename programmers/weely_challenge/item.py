34863be439fca643680f93c83100f367dd1a97c8
Junhyuck Ko <mrbartrns@naver.com>
2021-10-26 14:07:14 +0900

    Solving

--- programmers/weely_challenge/item.py
+++ programmers/weely_challenge/item.py
@@ -1,5 +1,11 @@
 # 11주차 아이템 줍기
-# TODO: 완벽하게 1의 모양이 나오도록 고치기
+import heapq
+
+
+def bfs(sy, sx, ey, ex, visited, graph):
+    q = []
+
+
 def draw_graph(graph, x1, y1, x2, y2):
     bx1, bx2 = x1, x2
     by1, by2 = y1, y2
@@ -7,13 +13,13 @@ def draw_graph(graph, x1, y1, x2, y2):
         bx1, bx2 = bx2, bx1
     if by2 < by1:
         by1, by2 = by2, by1
-    for i in range(bx1, bx2 + 1):
-        graph[by2][i] += 1
-    for i in range(by1, by2 + 1):
-        graph[i][bx2] += 1
-    for i in range(bx2 + 1, bx1, -1):
+    for i in range(bx1, bx2):
         graph[by1][i] += 1
-    for i in range(by2 + 1, by1, -1):
+    for i in range(by1, by2):
+        graph[i][bx2] += 1
+    for i in range(bx2, bx1, -1):
+        graph[by2][i] += 1
+    for i in range(by2, by1, -1):
         graph[i][bx1] += 1
 
 
@@ -22,10 +28,11 @@ def solution(rectangle, character_x, character_y, item_x, item_y):
     graph = [[0 for _ in range(10)] for _ in range(10)]
     for x1, y1, x2, y2 in rectangle:
         draw_graph(graph, x1, y1, x2, y2)
-        break
-    for i in range(10):
-        for j in range(10):
-            print(graph[i][j], end=" ")
+
+        for i in range(10):
+            for j in range(10):
+                print(graph[i][j], end=" ")
+            print()
         print()
     return answer
 
