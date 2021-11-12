1de03b2ca93f81edfd69704bc21a51a0cc91f6af
Junhyuck Ko <mrbartrns@naver.com>
2021-11-12 15:02:37 +0900

    add comment

--- jongman/dp/poly.py
+++ jongman/dp/poly.py
@@ -7,11 +7,14 @@ MOD = 10 * 1000 * 1000
 
 
 def poly(n, first):
+    # 모든 블록을 다 사용했다면 성공한 경우이므로 1을 반환한다.
     if n == first:
         return 1
+    # 이미 방문했다면, 수를 반환한다.
     if cache[n][first] > -1:
         return cache[n][first]
     cache[n][first] = 0
+    # 모든 경우에 대해서 완전 탐색한다.
     for second in range(1, n - first + 1):
         add = second + first - 1
         add *= poly(n - first, second)
@@ -28,5 +31,6 @@ for _ in range(T):
     s = 0
     for first in range(1, N + 1):
         s += poly(N, first)
+        # 다 더한후 MOD 나누는 것 잊지 않기
         s %= MOD
     print(s)
