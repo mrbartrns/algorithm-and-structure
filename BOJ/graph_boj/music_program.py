c84efba9840ac26b4c5f70ccd6b6e1cd3f333527
Junhyuck Ko <mrbartrns@naver.com>
2021-11-13 11:40:06 +0900

    Solve topological sort

---
+++ BOJ/graph_boj/music_program.py
@@ -0,0 +1,40 @@
+# BOJ 2623 음악 프로그램
+from collections import deque
+import sys
+
+sys.stdin = open("../input.txt", "r")
+si = sys.stdin.readline
+
+
+def topological_sort():
+    q = deque()
+    ret = []
+    for i in range(1, N + 1):
+        if indegree[i] == 0:
+            ret.append(i)
+            q.append(i)
+    while q:
+        cur = q.popleft()
+        for nxt in range(1, N + 1):
+            if adj[cur][nxt] == 1:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    ret.append(nxt)
+                    q.append(nxt)
+    if len(ret) < N:
+        return [0]
+    return ret
+
+
+N, M = map(int, si().split(" "))
+adj = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+indegree = [0] * (N + 1)
+for _ in range(M):
+    arr = list(map(int, si().split(" ")))
+    for i in range(2, len(arr)):
+        if adj[arr[i - 1]][arr[i]] == 0:
+            indegree[arr[i]] += 1
+            adj[arr[i - 1]][arr[i]] = 1
+orders = topological_sort()
+for order in orders:
+    print(order)
